a70c510bb2e0cb2ccc1610980d8981edf483e050
harjeet-pal-singh <visibleanimations@gmail.com>
2025-05-05 13:29:19 +0530

    Update ManageCouriers.jsx

--- src/pages/ManageCouriers.jsx
+++ src/pages/ManageCouriers.jsx
@@ -26,7 +26,7 @@ const ManageCouriers = () => {
   useEffect(() => {
     const fetchCouriers = async () => {
       try {
-        const response = await axios.get('http://localhost:5000/api/couriers', {
+        const response = await axios.get('https://singhcourierservice.vercel.app/api/couriers}', {
           headers: { Authorization: `Bearer ${token}` },
         });
         setCouriers(response.data);
@@ -39,7 +39,7 @@ const ManageCouriers = () => {
 
   const handleDelete = async (id) => {
     try {
-      const response = await axios.delete(`http://localhost:5000/api/couriers/${id}`, {
+      const response = await axios.delete(`https://singhcourierservice.vercel.app/api/couriers/${id}`, {
         headers: { Authorization: `Bearer ${token}` },
       });
       setCouriers((prev) => prev.filter((c) => c._id !== id));
@@ -78,7 +78,7 @@ const ManageCouriers = () => {
   const handleUpdate = async () => {
     try {
       await axios.put(
-        `http://localhost:5000/api/couriers/${editingCourierId}`,
+        `https://singhcourierservice.vercel.app/api/couriers/${editingCourierId}`,
         {
           sender: editFormData.sender,
           recipient: editFormData.recipient,
