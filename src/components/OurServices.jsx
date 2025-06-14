d541cad1ec9b8bcc966f74b422ed837f2d69220a
harjeet-pal-singh <visibleanimations@gmail.com>
2025-06-14 17:03:33 +0530

    Update OurServices.jsx

--- src/components/OurServices.jsx
+++ src/components/OurServices.jsx
@@ -31,7 +31,7 @@ const Services = () => {
           <div className="service-card" key={index}>
             <div className="service-icon">{service.icon}</div>
             <h3>{service.title}</h3>
-            <p>{service.description}</p>
+          
           </div>
         ))}
       </div>
